185922399734939f95b4f71d78e5039142a40d14
Aleksey Artamonov <aleksartamonov@gmail.com>
2018-03-28 18:07:13 +0300

    jupyter for yolo darkflow

---
+++ darkflow/run_jupyter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IMAGE=darkflow-gpu
+# execute docker run with nvidia driver and device
+docker run -it --device=/dev/nvidiactl \
+               --device=/dev/nvidia-uvm --device=/dev/nvidia0 --volume-driver nvidia-docker \
+               -v nvidia_driver_367.57:/usr/local/nvidia:ro \
+               $IMAGE \
+               /run_jupter.sh --allow-root
