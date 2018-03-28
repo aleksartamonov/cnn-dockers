d51f054862ca14b7e74512e663365e19720e49d3
Aleksey Artamonov <aleksartamonov@gmail.com>
2018-03-28 19:53:11 +0300

    driver version

--- darkflow/run_jupyter.sh
+++ darkflow/run_jupyter.sh
@@ -6,4 +6,4 @@ docker run -it --device=/dev/nvidiactl \
                --device=/dev/nvidia-uvm --device=/dev/nvidia0 --volume-driver nvidia-docker \
                -v nvidia_driver_384.111:/usr/local/nvidia:ro \
                $IMAGE \
-               /run_jupter.sh --allow-root
+               /run_jupyter.sh --allow-root
